29661e52c192cd1eb7e520eb67048265a80c0fab
Huilson J. Lorenzi <hill.winterclaw@gmail.com>
2024-09-02 21:49:17 -0300

    Update ok

--- crudjava/src/main/java/com/crudjava/crudjava/controller/ListaController.java
+++ crudjava/src/main/java/com/crudjava/crudjava/controller/ListaController.java
@@ -3,8 +3,11 @@ package com.crudjava.crudjava.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,6 +17,8 @@ import com.crudjava.crudjava.model.Conteudo;
 import com.crudjava.crudjava.repository.ConteudoRepository;
 
 import lombok.AllArgsConstructor;
+
+
 
 
 @RestController
@@ -29,12 +34,30 @@ public class ListaController {
         return repository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Conteudo> encontrarId(@PathVariable Long id) {
+        System.out.println("buscando id: "+id);        
+        return repository.findById(id).map(object -> ResponseEntity.ok().body(object))
+        .orElse(ResponseEntity.notFound().build());
+    }    
+
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Conteudo salvarConteudo(@RequestBody Conteudo conteudo){
         System.out.println("salvou!");
-        return repository.save(conteudo);        
-        //return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(conteudo));
+        return repository.save(conteudo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Conteudo> atualizarConteudo(@PathVariable Long id, @RequestBody Conteudo entity) {
+        return repository.findById(id).map(novo -> {
+            novo.setNome(entity.getNome());
+            novo.setCpf(entity.getCpf());
+            novo.setNumero(entity.getNumero());
+            Conteudo update = repository.save(novo);
+            return ResponseEntity.ok().body(update);
+        })
+        .orElse(ResponseEntity.notFound().build());
     }
 
 }
